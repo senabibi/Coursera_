352605e78a7fbd19b07729542a12dfcde2948e84
NURSENA BİTİRGEN <94117345+senabibi@users.noreply.github.com>
2023-11-04 21:54:24 +0300

    Create Assignment_1.py

---
+++ Algorithmic _Toolbox _University_of_California_San_Diego/Assignment_1.py
@@ -0,0 +1,9 @@
+# Read two digits from standard input
+num1 = int(input("Enter the first digit: "))
+num2 = int(input("Enter the second digit: "))
+
+# Calculate the sum of the two digits
+sum_of_digits = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum_of_digits)
